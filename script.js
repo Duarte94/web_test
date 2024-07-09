620b1a0d1806d885fe8bc42ce9c2639b054e9ffa
Duarte94 <duarte.cdf94@gmail.com>
2024-07-09 00:06:03 -0300

    Added slider with JS

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+const slider1 = document.getElementById("slider1");
+const slider2 = document.getElementById("slider2");
+const slider3 = document.getElementById("slider3");
+
+let sliders = [slider1,slider2,slider3];
+let currentIndex = 0;
+
 document.addEventListener("DOMContentLoaded", function() {
     if (document.title === "TechWave Solutions") {
         let servBtn = document.getElementsByClassName("servImg");
@@ -20,3 +27,13 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     }
 });
+
+document.addEventListener("DOMContentLoaded", sliderActive());
+
+function sliderActive() {
+    sliders.forEach(slider => slider.hidden = true);
+    sliders[currentIndex].hidden = false;
+    currentIndex = (currentIndex + 1) % sliders.length;
+}
+
+setInterval(sliderActive, 5000);
